f234ea9d21063a52434b37b1f309bd8afe555cb7
adriens <Adrien.Sales@gmail.com>
2020-12-12 09:38:13 +1100

    Fixes #3

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat = Join-Path $content 'vendir'
+
+Uninstall-BinFile `
+  -Name 'vendir' `
+  -Path $bat
